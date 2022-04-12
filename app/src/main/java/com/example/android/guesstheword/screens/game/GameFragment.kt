5f8ed20f91195f4981fe7f77fc653bf01f7a5942
mugishaEsther <mugesther@gmail.com>
2022-04-12 11:38:00 -0400

    first commit

---
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -0,0 +1,59 @@
+package com.example.android.guesstheword.screens.game
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import com.example.android.guesstheword.R
+import com.example.android.guesstheword.databinding.GameFragmentBinding
+
+/**
+ * Fragment where the game is played
+ */
+class GameFragment : Fragment() {
+    private lateinit var binding: GameFragmentBinding
+    private lateinit var viewModel: GameViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        // Inflate view and obtain an instance of the binding class
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.game_fragment,
+            container,
+            false
+        )
+
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+
+        viewModel.eventGameFinish.observe(viewLifecycleOwner, Observer {
+                hasFinished -> if (hasFinished) gameFinished()
+        })
+        binding.gameViewModel = viewModel
+
+        return binding.root
+
+    }
+    /**
+     * Called when the game is finished
+     */
+    private fun gameFinished() {
+        Toast.makeText(activity, "Game has just finished", Toast.LENGTH_SHORT).show()
+        val action = GameFragmentDirections.actionGameToScore()
+        action.score = viewModel.score.value ?: 0
+        NavHostFragment.findNavController(this).navigate(action)
+        viewModel.onGameFinishComplete()
+    }
+}
